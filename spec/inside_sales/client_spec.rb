218de1b21666636e9ba4ac6c23da0411c48cb39b
Michael Guterl <michael@diminishing.org>
2012-01-09 16:34:56 -0500

    Add specs for Client#login

--- spec/inside_sales/client_spec.rb
+++ spec/inside_sales/client_spec.rb
@@ -3,13 +3,43 @@ require 'spec_helper'
 describe InsideSales::Client do
 
   let(:client) {
-    client = InsideSales::Client.new(ENV['IS_SUBDOMAIN'],
-                                     ENV['IS_USERNAME'],
-                                     ENV['IS_PASSWORD'],
-                                     ENV['IS_TOKEN'])
+    client = build_client
     client.login
     client
   }
+
+  def build_client
+    InsideSales::Client.new(ENV['IS_SUBDOMAIN'],
+                            ENV['IS_USERNAME'],
+                            ENV['IS_PASSWORD'],
+                            ENV['IS_TOKEN'])
+  end
+
+  describe '#login' do
+    context 'when successful' do
+      use_vcr_cassette
+
+      it 'returns true' do
+        client = build_client
+        client.login.should be_true
+      end
+    end
+
+    context 'when unsuccessful' do
+      use_vcr_cassette
+
+      it 'raises an error' do
+        client = InsideSales::Client.new(ENV['IS_SUBDOMAIN'],
+                                         'bad',
+                                         'bad',
+                                         'bad')
+
+        expect {
+          client.login
+        }.to raise_error
+      end
+    end
+  end
 
   describe '#get_leads' do
 
